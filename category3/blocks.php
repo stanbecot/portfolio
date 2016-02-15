a38c7319a8273d4910b8f268f7f6c2f0e1d92be7
Stanislas Bécot <stan.becot2s@hotmail.fr>
2016-02-15 23:36:29 +0100

    sod

--- category3/blocks.php
+++ category3/blocks.php
@@ -1,49 +1,53 @@
-<div class="container blocks" id="category-3">
+<div class="container blocks" id="category-2">
 	<div class="row clearfix">
 
 		<!-- This is the beginning of the first block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/NW8AnRexUpo" frameborder="0" allowfullscreen color="white"></iframe>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/118115098&amp;auto_play=true&amp;hide_related=true&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true&amp;"></iframe>
 			</div>
 		</div>
-		<!-- And here's where it ends -->
+		<!-- And heres' where it ends -->
 
 		<!-- This is the beginning of the second block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/kOucrAFX810" frameborder="0" allowfullscreen></iframe>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/133039259&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 			</div>
 		</div>
-		<!-- And here's where it ends -->
+		<!-- And heres' where it ends -->
 
 		<!-- This is the beginning of the third block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/5eBCbUxMej4" frameborder="0" allowfullscreen></iframe>			</div>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/190908516&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			</div>
 		</div>
-		<!-- And here's where it ends -->
+		<!-- And heres' where it ends -->
 
 		<!-- This is the beginning of the fourth block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/ku46pW8ksTE" frameborder="0" allowfullscreen></iframe>			</div>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/176415558&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			</div>
 		</div>
-		<!-- And here's where it ends -->
+		<!-- And heres' where it ends -->
 
 		<!-- This is the beginning of the fifth block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/vwNceY7YkLk" frameborder="0" allowfullscreen></iframe>			</div>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/118115098&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			</div>
 		</div>
-		<!-- And here's where it ends -->
+		<!-- And heres' where it ends -->
 
 		<!-- This is the beginning of the sixth block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" src="https://www.youtube.com/embed/QeJqgw5CNyw" frameborder="0" allowfullscreen></iframe>			</div>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/194883698&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			</div>
 		</div>
-		<!-- And here's where it ends -->
+		<!-- And heres' where it ends -->
 
 		<!-- Here's where you should paste new blocks -->
 
