deb4b0d6a0973b35138baf623b4e8c00fdecb84d
Stanislas Bécot <stan.becot2s@hotmail.fr>
2016-02-15 23:45:53 +0100

    sodjjjfh

--- category3/blocks.php
+++ category3/blocks.php
@@ -12,7 +12,7 @@
 		<!-- This is the beginning of the second block of the mosaic -->
 		<div class="col-sm-4 column">
 			<div class="media-block">
-				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/133039259&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+				<iframe width="100%" height="100%" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/234594799&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 			</div>
 		</div>
 		<!-- And heres' where it ends -->
