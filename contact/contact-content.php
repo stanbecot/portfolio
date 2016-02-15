7a48cf282e730dac648b94bf83e402900c7c0b3c
Stanislas Bécot <stan.becot2s@hotmail.fr>
2016-02-15 23:55:25 +0100

    so

--- contact/contact-content.php
+++ contact/contact-content.php
@@ -3,22 +3,23 @@
 	<div class="text">
 
 			<!-- Here's the title of the contact -->
-			<h2>Pablo Faust</h2>
+			<h2>Stanislas Bécot</h2>
 
 			<!-- Here's the mail adress of the contact -->
-			<a href="mailto:pabfaust@gmail.com" target="_top"><h3><strong>pabfaust@gmail.com</strong></h3></a>
+			<a href="mailto:pabfaust@gmail.com" target="_top"><h3><strong>stan.becot2s@hotmail.fr</strong></h3></a>
 
 			<!-- Here's where you can write whatever you want, such as the adress of the contact, or the phone number -->
-			<h4>Here, you can put your phone number, but I won't give you mine !<br><br>
+			<!-- <h4>Here, you can put your phone number, but I won't give you mine !<br><br> -->
 
 
 			<strong>See more:</strong></h4>
 
 			<!-- Here are the social icons -->
-			<a href="https://www.facebook.com/pabfaust" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>
-			<a href="https://twitter.com/PabFaust" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a>
-			<a href="https://www.behance.net/pablofaust" target="_blank"><i class="fa fa-behance-square fa-2x"></i></a>
-			<a href="https://soundcloud.com/pablo-faust"><i class="fa fa-soundcloud fa-2x"></i></a>
+			<a href="https://www.facebook.com/biankalovesyou" target="_blank"><i class="fa fa-facebook-official fa-2x"></i></a>
+			<!-- <a href="https://twitter.com/PabFaust" target="_blank"><i class="fa fa-twitter-square fa-2x"></i></a> -->
+			<!-- <a href="https://www.behance.net/BIANKA7" target="_blank"><i class="fa fa-behance-square fa-2x"></i></a> -->
+			<a href="https://soundcloud.com/biankamusic"><i class="fa fa-soundcloud fa-2x"></i></a>
+			<a href="https://instagram.com/bianka7"><i class="fa fa-instagram fa-2x"></i></a>
 			<!-- You should paste here the social icons you want to agregate -->
 
 	</div>
